cd5bd125476c7f22ec75971561170aa0349c8acd
Sam Lau <samlaudev@gmail.com>
2021-10-29 12:20:27 +0800

    create project

---
+++ Moments/Moments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments
+//
+//  Created by Sam Lau on 2021/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
