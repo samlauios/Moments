089342a60bc8021b8e99a9c65764064ea90932cb
Sam Lau <samlaudev@gmail.com>
2021-11-24 15:01:05 +0800

    Add Firebase SDK support

--- Moments/Moments/AppDelegate.swift
+++ Moments/Moments/AppDelegate.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        onLaunch()
+
         return true
     }
 
@@ -30,7 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+private extension AppDelegate {
+    func onLaunch() {
+        FirebaseApp.configure()
+    }
+}
