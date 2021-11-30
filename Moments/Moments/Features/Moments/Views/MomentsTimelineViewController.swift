42329b06f68223d836d86965d9f00eb022e42a9d
samlauios <samlautest13@gmail.com>
2021-11-30 17:40:31 +0800

    Add entrance

---
+++ Moments/Moments/Features/Moments/Views/MomentsTimelineViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MomentsTimelineViewController.swift
+//  Moments
+//
+//  Created by Sam Lau on 2021/11/30.
+//
+
+import UIKit
+
+class MomentsTimelineViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "朋友圈"
+    }
+}
---
+++ Moments/Moments/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Moments
+//
+//  Created by Sam Lau on 2021/11/30.
+//
+
+import UIKit
+
+_ = UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
